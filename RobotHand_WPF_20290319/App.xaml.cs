35a38c6478d546610946119410fd6fa034948dfc
ZXH <1614295872@qq.com>
2026-03-27 14:05:26 +0800

    机械臂的wpf版本

---
+++ RobotHand_WPF_20290319/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+using Prism.DryIoc;
+using Prism.Ioc;
+using RobotHand_WPF_20290319.ViewModels;
+using RobotHand_WPF_20290319.Views;
+using System.Windows;
+
+namespace RobotHand_WPF_20290319
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainWindow, MainWindowViewModel>();
+
+        }
+    }
+
+}
---
+++ RobotHand_WPF_20290319/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotHand_WPF_20290319.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+        #region 属性
+        private bool isOpenMainLeftForm;
+        public bool IsOpenMainLeftForm
+        {
+            get { return isOpenMainLeftForm; }
+            set { SetProperty(ref isOpenMainLeftForm, value); }
+        }
+        #endregion
+    }
+}
